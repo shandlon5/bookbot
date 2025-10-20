5468562e46f5e3f5e4ec6e543bf0d10ef081e57b
shandlon5 <shandlon5@gmail.com>
2025-10-20 11:04:09 -0400

    B: counting words

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
-         print(f.read())
-         return f.read()
+         text = f.read()
+         return text 
 
-text = get_book_text("books/frankenstein.txt")
+def word_count(text):
+    num_words = text.split()
+    count = len(num_words)
+    print(f"Found {count} total words")
+    return count
+
+def main():
+# Step 1: Get the text from your file
+    text = get_book_text("books/frankenstein.txt")  # Make sure the path is correct
+
+# Step 2: Count the words in that text
+    total_words = word_count(text)
+
+main()
+
+
 
